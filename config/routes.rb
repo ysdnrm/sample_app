06c3928c50c91dc374f0462474195cc550803fc6
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-06-07 07:46:20 +0000

    [Finish] 3-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,27 @@
+# ルーディングの設定する場所
+# （modelとviewの住所）
 Rails.application.routes.draw do
   # HTTPリクエスト
   # どのURLを指定されたときに、どのページを表示するか決めるところ
   # GETはデータの取得をするためのHTTPメソッド（新しいページを取得する場合など）
 
-  # 　↓は「'lists/new' => 'lists#new'」の省略版
+  #  ↓は「'lists/new' => 'lists#new'」の省略版
+  #  ↓
   get 'lists/new'
 
   get '/top' => 'homes#top'
+
   # POSTはデータを新規保存するためのHTTPメソッド（投稿する場合など）
   post 'lists' => 'lists#create'
+
   # index.html.erbで作成したページを表示するためにルーディングを設定
   get 'lists' => 'lists#index'
 
-  get 'lists/show'
+  # 詳細画面では、どの投稿データを表示するかを判別するために、投稿データのidもURLに含めます。
+  # 詳細画面で呼び出される投稿データは、URLの/lists/:id内の:idで判別します。
+  # この:idは、アクション内にparams[:id]と記述することで取得できます。
+  get 'lists/:id' => 'lists#show' , as: 'list'
+  # as: 'list' は'lists#show' の設定の住所に「list」という名前をつけたということ
 
   get 'lists/edit'
 
